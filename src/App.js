d42949d844be0ec75133752a08170fd6ece1d6f2
Kyrylo <fresh.js.ftd.@gmail.com>
2024-01-17 18:38:15 +0200

    Add conditional rendering examples

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import UserCard from './components/UserCard';
 
-const user = {
+const user1 = {
   id: 1,
-  name : 'User'
+  name : 'User',
+  isVip: false
 }
+
+
+const user2 = {
+  id: 2,
+  name : 'User2',
+  isVip: true
+}
+
 
 function App() {
   return (
     <div>
-      <UserCard user={user}/>
+      <UserCard user={user1}/>
+      <UserCard user={user2}/>
     </div>
   );
 }
--- src/components/UserCard/index.jsx
+++ src/components/UserCard/index.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import './style.css';
 
 const UserCard = (props) => {
   const {
-    user: { id, name },
+    user: { id, name, isVip },
   } = props;
 
+  // let className;
+
+  // switch (name) {
+  //   case 1: {
+  //     className = 'adsad';
+  //     break;
+  //   } 
+  //   case 2: {
+  //     className = '2132143';
+  //     break;
+  //   }
+  // }
+
   return (
-    <div>
+    <div className={`userContainer ${isVip ? 'vip' : ''}`}>
+      <h1 title={isVip ? 'VIP' : 'NOT VIP, RECONSIDER'}>
+        User is {isVip ? 'VIP' : 'not VIP'}
+      </h1>
+      {!isVip && <p>Become VIP NOW!!!!!!</p>}
       <p>{name}</p>
       <p>{id}</p>
+      <div>{isVip ? <p>You already VIP</p> : <button>Become VIP</button>}</div>
     </div>
   );
 };
