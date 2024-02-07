a1be548ddd8e9bcc3653533c48e4646d34c81d30
Kyrylo <fresh.js.ftd.@gmail.com>
2024-02-07 19:16:46 +0200

    Add react-router-dom example

--- src/App.js
+++ src/App.js
@@ -2,18 +2,47 @@ import { useState } from 'react';
 import './App.css';
 import { UserContext } from './contexts';
 import ContextConsumer from './components/ContextConsumer';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   const [user, setUser] = useState({
     id: 123214234,
-    name: 'Test User Name'
+    name: 'Test User Name',
   });
 
   return (
-    <UserContext.Provider value={user}>
-      <ContextConsumer />
-    </UserContext.Provider>
+    // <BrowserRouter>
+    <>
+      <UserContext.Provider value={user}>
+        <ContextConsumer />
+      </UserContext.Provider>
+      <header>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/nothome'>Not Home</Link></li>
+        </ul>
+      </header>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/nothome' component={NotHomePage} />
+        <Route path='/profile/:userId' component={NotHomePage} />
+      </Switch>
+    </>
+    //  </BrowserRouter> 
   );
+}
+
+function HomePage (props) {
+  console.log(props);
+  return <div>
+    <h2>HOME PAGE</h2>
+  </div>
+}
+
+function NotHomePage () {
+  return <div>
+    <h2>NOT HOME PAGE</h2>
+  </div>
 }
 
 export default App;
