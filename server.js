80bca4285293e5e7f6479546d3a576bf9eb37557
Kyrylo <fresh.js.ftd.@gmail.com>
2024-03-13 18:42:10 +0200

    Add basic middlewares examples

--- server.js
+++ server.js
@@ -11,14 +11,42 @@ app.get('/phones', async (request, response, next) => {
   response.send({ data: phones });
 });
 
-app.get('/cars', async (request, response, next) => {
+// GET http://localhost:5000/phones/5
+app.get('/phones/:id', async (request, response, next) => {
+  console.log(request.params.id); // 5
+  // складний код
+  const phones = [{ id: 0 }, { id: 1 }];
+
+  response.send({ data: phones });
+});
+
+const middleware1 = async (req, res, next) => {
+  // міддлвер 1
+  const result = Math.random() > 0.5;
+  console.log(result);
+
+  req.data = Math.random();
+
+  if (result) {
+    next();
+  } else {
+    res.send('middleware validation failure');
+    // ERROR
+    // res.send('middleware validation failure');
+  }
+};
+
+// міддлвери - проміжні обробники на певному запиті
+app.get('/cars', middleware1, async (request, response, next) => {
   // складний код
   const cars = [{ id: 12320 }, { id: 112322 }];
 
-  response.send({ data: cars });
+  response.send({ data: cars, number: request.data });
 });
 
-app.post('/phones', async (req, res, next) => {
+// Content-type: application / json
+app.post('/phones', express.json(), async (req, res, next) => {
+  const { body } = req;
   const newPhone = { id: 2 };
 
   res.send({ data: newPhone });
